44c91e198c4ca520c7281749a27266caf7538c2a
Omar Polo <op@omarpolo.com>
2025-01-07 14:44:41 +0000

    convert ListBlobs to an iterator

--- repository/state/state.go
+++ repository/state/state.go
@@ -20,6 +20,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"iter"
 	"sync"
 	"time"
 
@@ -607,9 +608,8 @@ func (st *State) DeleteSnapshot(snapshotChecksum objects.Checksum) error {
 	return nil
 }
 
-func (st *State) ListBlobs(Type packfile.Type) <-chan objects.Checksum {
-	ch := make(chan objects.Checksum)
-	go func() {
+func (st *State) ListBlobs(Type packfile.Type) iter.Seq[objects.Checksum] {
+	return func(yield func(objects.Checksum) bool) {
 		var mapPtr *map[uint64]Location
 		var mtx *sync.Mutex
 		switch Type {
@@ -646,11 +646,11 @@ func (st *State) ListBlobs(Type packfile.Type) <-chan objects.Checksum {
 		mtx.Unlock()
 
 		for _, checksum := range blobsList {
-			ch <- checksum
+			if !yield(checksum) {
+				return
+			}
 		}
-		close(ch)
-	}()
-	return ch
+	}
 }
 
 func (st *State) ListSnapshots() <-chan objects.Checksum {
