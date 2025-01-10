5c12203c2b41a64939ec985530ddba5bba52fd51
Dejan Filipovic <sayoun@free.fr>
2025-01-10 21:47:58 +0000

    refactor testing package
    create sample behaviors with fake values to be used in tests

---
+++ testing/storage.go
@@ -0,0 +1,46 @@
+package testing
+
+import (
+	"time"
+
+	"github.com/PlakarKorp/plakar/chunking"
+	"github.com/PlakarKorp/plakar/compression"
+	"github.com/PlakarKorp/plakar/encryption"
+	"github.com/PlakarKorp/plakar/hashing"
+	"github.com/PlakarKorp/plakar/packfile"
+	"github.com/PlakarKorp/plakar/storage"
+	"github.com/google/uuid"
+)
+
+type configurationOptions struct {
+	*storage.Configuration
+}
+
+type ConfigurationOptions func(o *storage.Configuration)
+
+func WithConfigurationCompression(compression *compression.Configuration) ConfigurationOptions {
+	return func(o *storage.Configuration) {
+		o.Compression = compression
+	}
+}
+
+func NewConfiguration(opts ...ConfigurationOptions) *storage.Configuration {
+	conf := storage.Configuration{
+		Version:      storage.VERSION,
+		Timestamp:    time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC),
+		RepositoryID: uuid.MustParse("00ff0000-0000-4000-a000-000000000001"),
+
+		Packfile: *packfile.DefaultConfiguration(),
+		Chunking: *chunking.DefaultConfiguration(),
+		Hashing:  *hashing.DefaultConfiguration(),
+
+		Compression: compression.DefaultConfiguration(),
+		Encryption:  encryption.DefaultConfiguration(),
+	}
+
+	for _, f := range opts {
+		f(&conf)
+	}
+
+	return &conf
+}
