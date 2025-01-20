48b36a4f05d35dec590e376bb86dbc80445307ae
Dejan Filipovic <sayoun@free.fr>
2025-01-20 22:21:28 +0100

    add missing appcontext tests

--- appcontext/appcontext_test.go
+++ appcontext/appcontext_test.go
@@ -2,10 +2,21 @@ package appcontext
 
 import (
 	"testing"
+
+	"github.com/PlakarKorp/plakar/caching"
+	"github.com/PlakarKorp/plakar/encryption/keypair"
+	"github.com/PlakarKorp/plakar/logging"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
 )
 
 func TestContext_SettersAndGetters(t *testing.T) {
 	ctx := NewAppContext()
+
+	defaultLogger := logging.NewLogger(nil, nil)
+	defaultCachingManager := caching.NewManager("/tmp/test_plakar")
+	defaultKeyPair, err := keypair.Generate()
+	require.NoError(t, err)
 
 	tests := []struct {
 		name     string
@@ -101,14 +112,94 @@ func TestContext_SettersAndGetters(t *testing.T) {
 			getter:   func() interface{} { return ctx.GetProcessID() },
 			expected: 12345,
 		},
+		{
+			name: "SetKeyringDir",
+			setter: func() {
+				ctx.SetKeyringDir("/keyring/dir")
+			},
+			getter:   func() interface{} { return ctx.GetKeyringDir() },
+			expected: "/keyring/dir",
+		},
+		{
+			name: "SetIdentity",
+			setter: func() {
+				ctx.SetIdentity(uuid.MustParse("123e4567-e89b-12d3-a456-426614174000"))
+			},
+			getter:   func() interface{} { return ctx.GetIdentity() },
+			expected: uuid.MustParse("123e4567-e89b-12d3-a456-426614174000"),
+		},
+		{
+			name: "SetKeypair",
+			setter: func() {
+				ctx.SetKeypair(defaultKeyPair)
+			},
+			getter:   func() interface{} { return ctx.GetKeypair() },
+			expected: defaultKeyPair,
+		},
+		{
+			name: "SetPlakarClient",
+			setter: func() {
+				ctx.SetPlakarClient("plakar-client")
+			},
+			getter:   func() interface{} { return ctx.GetPlakarClient() },
+			expected: "plakar-client",
+		},
+		{
+			name: "SetMaxConcurrency",
+			setter: func() {
+				ctx.SetMaxConcurrency(10)
+			},
+			getter:   func() interface{} { return ctx.GetMaxConcurrency() },
+			expected: 10,
+		},
+		{
+			name: "SetCache",
+			setter: func() {
+				ctx.SetCache(defaultCachingManager)
+			},
+			getter:   func() interface{} { return ctx.GetCache() },
+			expected: defaultCachingManager,
+		},
+		{
+			name: "SetLogger",
+			setter: func() {
+				ctx.SetLogger(defaultLogger)
+			},
+			getter:   func() interface{} { return ctx.GetLogger() },
+			expected: defaultLogger,
+		},
+		{
+			name: "SetCWD",
+			setter: func() {
+				ctx.SetCWD("/current/working/dir")
+			},
+			getter:   func() interface{} { return ctx.GetCWD() },
+			expected: "/current/working/dir",
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			test.setter()
 			if result := test.getter(); result != test.expected {
-				t.Errorf("%s failed: expected %v, got %v", test.name, test.expected, result)
+				t.Errorf("%s failed: expected %#v, got %#v", test.name, test.expected, result)
 			}
 		})
+	}
+}
+
+func TestAppContextCloseEvents(t *testing.T) {
+	ctx := NewAppContext()
+	events := ctx.Events()
+	if events == nil {
+		t.Errorf("events is nil")
+	}
+	ctx.Close()
+	// Check if events is closed
+	select {
+	case <-events.Listen():
+		t.Errorf("events is not closed")
+	default:
+		// events is closed
 	}
 }
