5bc2f46e795e1f5c5b19ebb4e720eba095b7985a
Gilles Chehade <gilles@poolp.org>
2025-01-28 23:53:54 +0100

    fix copyright and package name

--- cmd/plakar/subcommands/locate/locate.go
+++ cmd/plakar/subcommands/locate/locate.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Gilles Chehade <gilles@poolp.org>
+ * Copyright (c) 2025 Gilles Chehade <gilles@poolp.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-package ls
+package locate
 
 import (
 	"flag"
