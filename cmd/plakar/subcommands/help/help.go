90ac6bc2651f025290921fbc3b1c37d253f9c97b
Omar Polo <op@omarpolo.com>
2025-02-01 11:01:18 +0000

    move doc sync script to helper script

--- cmd/plakar/subcommands/help/help.go
+++ cmd/plakar/subcommands/help/help.go
@@ -74,13 +74,3 @@ func cmd_help(ctx *appcontext.AppContext, _ *repository.Repository, args []strin
 
 	return 1, err
 }
-
-// to rebuild documentation, run:
-/*
-find ../ -type f -name '*.1' -exec sh -c '
-  for file; do
-    base=$(basename "$file" .1)
-    mandoc -T markdown "$file" > "docs/$base.md"
-  done
-' sh {} +
-*/
