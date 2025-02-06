a1a6ec39b9bf2145b6480eb8ac1bc77b3d26e056
Gilles Chehade <gilles@poolp.org>
2025-02-06 14:34:59 +0100

    do hmac-resolved blob lookups on everything but snapshot resources

--- repository/repository.go
+++ repository/repository.go
@@ -470,9 +470,12 @@ func (r *Repository) DeletePackfile(checksum objects.Checksum) error {
 func (r *Repository) GetBlob(Type resources.Type, checksum objects.Checksum) (io.ReadSeeker, error) {
 	t0 := time.Now()
 	defer func() {
-		r.Logger().Trace("repository", "GetBlob(%x): %s", checksum, time.Since(t0))
+		r.Logger().Trace("repository", "GetBlob(%s, %x): %s", Type, checksum, time.Since(t0))
 	}()
 
+	if Type != resources.RT_SNAPSHOT {
+		checksum = r.ChecksumHMAC(checksum[:])
+	}
 	packfileChecksum, offset, length, exists := r.state.GetSubpartForBlob(Type, checksum)
 	if !exists {
 		return nil, ErrPackfileNotFound
@@ -489,9 +492,12 @@ func (r *Repository) GetBlob(Type resources.Type, checksum objects.Checksum) (io
 func (r *Repository) BlobExists(Type resources.Type, checksum objects.Checksum) bool {
 	t0 := time.Now()
 	defer func() {
-		r.Logger().Trace("repository", "BlobExists(%x): %s", checksum, time.Since(t0))
+		r.Logger().Trace("repository", "BlobExists(%s, %x): %s", Type, checksum, time.Since(t0))
 	}()
 
+	if Type != resources.RT_SNAPSHOT {
+		checksum = r.ChecksumHMAC(checksum[:])
+	}
 	return r.state.BlobExists(Type, checksum)
 }
 
