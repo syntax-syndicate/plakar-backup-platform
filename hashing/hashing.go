f50ef83f8e9a95879d96bcc723367131b5b4d2d2
Gilles Chehade <gilles@poolp.org>
2025-02-08 00:26:22 +0100

    add support for BLAKE3 and switch from SHA256 to BLAKE3 as the default hashing algorithm

--- hashing/hashing.go
+++ hashing/hashing.go
@@ -5,6 +5,8 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"hash"
+
+	"github.com/zeebo/blake3"
 )
 
 type Configuration struct {
@@ -13,7 +15,8 @@ type Configuration struct {
 }
 
 func DefaultConfiguration() *Configuration {
-	configuration, _ := LookupDefaultConfiguration("SHA256")
+	//configuration, _ := LookupDefaultConfiguration("SHA256")
+	configuration, _ := LookupDefaultConfiguration("BLAKE3")
 	return configuration
 }
 
@@ -22,6 +25,11 @@ func LookupDefaultConfiguration(algorithm string) (*Configuration, error) {
 	case "SHA256":
 		return &Configuration{
 			Algorithm: "SHA256",
+			Bits:      256,
+		}, nil
+	case "BLAKE3":
+		return &Configuration{
+			Algorithm: "BLAKE3",
 			Bits:      256,
 		}, nil
 	default:
@@ -33,6 +41,8 @@ func GetHasher(name string) hash.Hash {
 	switch name {
 	case "SHA256":
 		return sha256.New()
+	case "BLAKE3":
+		return blake3.New()
 	default:
 		return nil
 	}
@@ -42,6 +52,8 @@ func GetHasherHMAC(name string, secret []byte) hash.Hash {
 	switch name {
 	case "SHA256":
 		return hmac.New(sha256.New, secret)
+	case "BLAKE3":
+		return hmac.New(func() hash.Hash { return blake3.New() }, secret)
 	default:
 		return nil
 	}
