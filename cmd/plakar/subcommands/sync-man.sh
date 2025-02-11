aa7c5d86913cd7eda9d21187753440239e3f9240
Omar Polo <op@omarpolo.com>
2025-02-11 14:13:40 +0000

    sync-man: hardcode the operating system

--- cmd/plakar/subcommands/sync-man.sh
+++ cmd/plakar/subcommands/sync-man.sh
@@ -3,6 +3,6 @@
 find . -type f -iname \*.1 -exec sh -c '
 	for file; do
 		base=$(basename "$file" .1)
-		mandoc -T markdown "$file" > "help/docs/$base.md"
+		mandoc -I os=Plakar -T markdown "$file" > "help/docs/$base.md"
 	done
 ' sh {} +
