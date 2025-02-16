69d101c717db9d5d7bd6c3cb112dd4fa1e4aca92
Gilles Chehade <gilles@poolp.org>
2025-02-16 12:34:26 +0100

    adapt check to use filters

--- cmd/plakar/subcommands/check/check.go
+++ cmd/plakar/subcommands/check/check.go
@@ -19,6 +19,7 @@ package check
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/PlakarKorp/plakar/appcontext"
 	"github.com/PlakarKorp/plakar/cmd/plakar/subcommands"
@@ -33,6 +34,16 @@ func init() {
 }
 
 func parse_cmd_check(ctx *appcontext.AppContext, repo *repository.Repository, args []string) (subcommands.Subcommand, error) {
+	var opt_name string
+	var opt_category string
+	var opt_environment string
+	var opt_perimeter string
+	var opt_job string
+	var opt_tag string
+	var opt_before string
+	var opt_since string
+	var opt_latest bool
+
 	var opt_concurrency uint64
 	var opt_fastCheck bool
 	var opt_noVerify bool
@@ -46,25 +57,81 @@ func parse_cmd_check(ctx *appcontext.AppContext, repo *repository.Repository, ar
 	}
 
 	flags.Uint64Var(&opt_concurrency, "concurrency", uint64(ctx.MaxConcurrency), "maximum number of parallel tasks")
+	flags.StringVar(&opt_name, "name", "", "filter by name")
+	flags.StringVar(&opt_category, "category", "", "filter by category")
+	flags.StringVar(&opt_environment, "environment", "", "filter by environment")
+	flags.StringVar(&opt_perimeter, "perimeter", "", "filter by perimeter")
+	flags.StringVar(&opt_job, "job", "", "filter by job")
+	flags.StringVar(&opt_tag, "tag", "", "filter by tag")
+	flags.StringVar(&opt_before, "before", "", "filter by date")
+	flags.StringVar(&opt_since, "since", "", "filter by date")
+	flags.BoolVar(&opt_latest, "latest", false, "use latest snapshot")
 	flags.BoolVar(&opt_noVerify, "no-verify", false, "disable signature verification")
 	flags.BoolVar(&opt_fastCheck, "fast", false, "enable fast checking (no digest verification)")
 	flags.BoolVar(&opt_quiet, "quiet", false, "suppress output")
 	flags.Parse(args)
 
+	var err error
+
+	var beforeDate time.Time
+	if opt_before != "" {
+		beforeDate, err = utils.ParseTimeFlag(opt_before)
+		if err != nil {
+			return nil, fmt.Errorf("invalid date format: %s", opt_before)
+		}
+	}
+
+	var sinceDate time.Time
+	if opt_since != "" {
+		sinceDate, err = utils.ParseTimeFlag(opt_since)
+		if err != nil {
+			return nil, fmt.Errorf("invalid date format: %s", opt_since)
+		}
+	}
+
+	if flags.NArg() != 0 {
+		if opt_name != "" || opt_category != "" || opt_environment != "" || opt_perimeter != "" || opt_job != "" || opt_tag != "" || !beforeDate.IsZero() || !sinceDate.IsZero() {
+			ctx.GetLogger().Warn("snapshot specified, filters will be ignored")
+		}
+	}
+
 	return &Check{
 		RepositoryLocation: repo.Location(),
 		RepositorySecret:   ctx.GetSecret(),
-		Concurrency:        opt_concurrency,
-		FastCheck:          opt_fastCheck,
-		NoVerify:           opt_noVerify,
-		Quiet:              opt_quiet,
-		Snapshots:          flags.Args(),
+
+		OptBefore: beforeDate,
+		OptSince:  sinceDate,
+		OptLatest: opt_latest,
+
+		OptName:        opt_name,
+		OptCategory:    opt_category,
+		OptEnvironment: opt_environment,
+		OptPerimeter:   opt_perimeter,
+		OptJob:         opt_job,
+		OptTag:         opt_tag,
+
+		Concurrency: opt_concurrency,
+		FastCheck:   opt_fastCheck,
+		NoVerify:    opt_noVerify,
+		Quiet:       opt_quiet,
+		Snapshots:   flags.Args(),
 	}, nil
 }
 
 type Check struct {
 	RepositoryLocation string
 	RepositorySecret   []byte
+
+	OptBefore time.Time
+	OptSince  time.Time
+	OptLatest bool
+
+	OptName        string
+	OptCategory    string
+	OptEnvironment string
+	OptPerimeter   string
+	OptJob         string
+	OptTag         string
 
 	Concurrency uint64
 	FastCheck   bool
@@ -82,8 +149,39 @@ func (cmd *Check) Execute(ctx *appcontext.AppContext, repo *repository.Repositor
 
 	var snapshots []string
 	if len(cmd.Snapshots) == 0 {
-		for snapshotID := range repo.ListSnapshots() {
-			snapshots = append(snapshots, fmt.Sprintf("%x", snapshotID))
+		locateOptions := utils.NewDefaultLocateOptions()
+		locateOptions.MaxConcurrency = ctx.MaxConcurrency
+		locateOptions.SortOrder = utils.LocateSortOrderDescending
+
+		locateOptions.Before = cmd.OptBefore
+		locateOptions.Since = cmd.OptSince
+		locateOptions.Latest = cmd.OptLatest
+
+		if cmd.OptName != "" {
+			locateOptions.Name = cmd.OptName
+		}
+		if cmd.OptCategory != "" {
+			locateOptions.Category = cmd.OptCategory
+		}
+		if cmd.OptEnvironment != "" {
+			locateOptions.Environment = cmd.OptEnvironment
+		}
+		if cmd.OptPerimeter != "" {
+			locateOptions.Perimeter = cmd.OptPerimeter
+		}
+		if cmd.OptJob != "" {
+			locateOptions.Job = cmd.OptJob
+		}
+		if cmd.OptTag != "" {
+			locateOptions.Tag = cmd.OptTag
+		}
+
+		snapshotIDs, err := utils.LocateSnapshotIDs(repo, locateOptions)
+		if err != nil {
+			return 1, fmt.Errorf("ls: could not fetch snapshots list: %w", err)
+		}
+		for _, snapshotID := range snapshotIDs {
+			snapshots = append(snapshots, fmt.Sprintf("%x:/", snapshotID))
 		}
 	} else {
 		snapshots = cmd.Snapshots
