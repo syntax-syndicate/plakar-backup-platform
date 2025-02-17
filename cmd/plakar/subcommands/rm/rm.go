236c2141e913fb6a26a4cf66314bb377cb9334eb
Gilles Chehade <gilles@poolp.org>
2025-02-17 22:45:30 +0100

    remove unncessary checks

--- cmd/plakar/subcommands/rm/rm.go
+++ cmd/plakar/subcommands/rm/rm.go
@@ -142,24 +142,12 @@ func (cmd *Rm) Execute(ctx *appcontext.AppContext, repo *repository.Repository) 
 		locateOptions.Since = cmd.OptSince
 		locateOptions.Latest = cmd.OptLatest
 
-		if cmd.OptName != "" {
-			locateOptions.Name = cmd.OptName
-		}
-		if cmd.OptCategory != "" {
-			locateOptions.Category = cmd.OptCategory
-		}
-		if cmd.OptEnvironment != "" {
-			locateOptions.Environment = cmd.OptEnvironment
-		}
-		if cmd.OptPerimeter != "" {
-			locateOptions.Perimeter = cmd.OptPerimeter
-		}
-		if cmd.OptJob != "" {
-			locateOptions.Job = cmd.OptJob
-		}
-		if cmd.OptTag != "" {
-			locateOptions.Tag = cmd.OptTag
-		}
+		locateOptions.Name = cmd.OptName
+		locateOptions.Category = cmd.OptCategory
+		locateOptions.Environment = cmd.OptEnvironment
+		locateOptions.Perimeter = cmd.OptPerimeter
+		locateOptions.Job = cmd.OptJob
+		locateOptions.Tag = cmd.OptTag
 
 		snapshotIDs, err := utils.LocateSnapshotIDs(repo, locateOptions)
 		if err != nil {
