ae6f50a05a81fea3b03c04440a9db29fe614e29a
Omar Polo <op@omarpolo.com>
2025-02-18 12:56:23 +0000

    add some helper functions

--- snapshot/vfs/errors.go
+++ snapshot/vfs/errors.go
@@ -37,6 +37,12 @@ func ErrorItemFromBytes(bytes []byte) (*ErrorItem, error) {
 	return e, err
 }
 
+func ErrorNodeFromBytes(bytes []byte) (*btree.Node[string, objects.MAC, objects.MAC], error) {
+	var e btree.Node[string, objects.MAC, objects.MAC]
+	err := msgpack.Unmarshal(bytes, &e)
+	return &e, err
+}
+
 func NewErrorItem(path, error string) *ErrorItem {
 	return &ErrorItem{
 		Version: versioning.FromString(VFS_ERROR_VERSION),
