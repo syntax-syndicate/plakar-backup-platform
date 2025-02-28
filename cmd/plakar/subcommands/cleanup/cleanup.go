4dd8e7b3f3cce6f1f4c113a39f00ce8e92b86545
Mathieu Masson <mathieu.masson@plakar.io>
2025-02-28 11:33:54 +0100

    maintaince: A bit of refactoring.
    * We need more context for the upcoming lock work, so let's move things
    around a bit.

--- cmd/plakar/subcommands/cleanup/cleanup.go
+++ cmd/plakar/subcommands/cleanup/cleanup.go
@@ -53,6 +53,9 @@ func parse_cmd_cleanup(ctx *appcontext.AppContext, repo *repository.Repository, 
 type Cleanup struct {
 	RepositoryLocation string
 	RepositorySecret   []byte
+
+	repository     *repository.Repository
+	maintainanceID objects.MAC
 }
 
 func (cmd *Cleanup) Name() string {
@@ -60,9 +63,9 @@ func (cmd *Cleanup) Name() string {
 }
 
 // Builds the local cache of snapshot -> packfiles
-func updateCache(ctx *appcontext.AppContext, repo *repository.Repository, cache *caching.MaintainanceCache) error {
-	for snapshotID := range repo.ListSnapshots() {
-		snapshot, err := snapshot.Load(repo, snapshotID)
+func (cmd *Cleanup) updateCache(ctx *appcontext.AppContext, cache *caching.MaintainanceCache) error {
+	for snapshotID := range cmd.repository.ListSnapshots() {
+		snapshot, err := snapshot.Load(cmd.repository, snapshotID)
 		if err != nil {
 			return err
 		}
@@ -98,7 +101,7 @@ func updateCache(ctx *appcontext.AppContext, repo *repository.Repository, cache 
 
 	// While ListSnapshots doesn't return deleted snapshots, we still need to
 	// go over them to remove previously added one to our local cache.
-	for snapshotID := range repo.ListDeletedSnapShots() {
+	for snapshotID := range cmd.repository.ListDeletedSnapShots() {
 		ok, err := cache.HasSnapshot(snapshotID)
 		if err != nil {
 			return err
@@ -116,26 +119,16 @@ func updateCache(ctx *appcontext.AppContext, repo *repository.Repository, cache 
 	return nil
 }
 
-func colourPass(ctx *appcontext.AppContext, repo *repository.Repository, cache *caching.MaintainanceCache) error {
+func (cmd *Cleanup) colourPass(ctx *appcontext.AppContext, cache *caching.MaintainanceCache) error {
 	var packfiles map[objects.MAC]struct{} = make(map[objects.MAC]struct{})
 
-	for packfileMAC := range repo.ListPackfiles() {
+	for packfileMAC := range cmd.repository.ListPackfiles() {
 		if !cache.HasPackfile(packfileMAC) {
 			packfiles[packfileMAC] = struct{}{}
 		}
 	}
 
-	// This random id generation for non snapshot state should probably be encapsulated somewhere.
-	var identifier objects.MAC
-	n, err := rand.Read(identifier[:])
-	if err != nil {
-		return err
-	}
-	if n != len(identifier) {
-		return io.ErrShortWrite
-	}
-
-	sc, err := repo.AppContext().GetCache().Scan(identifier)
+	sc, err := cmd.repository.AppContext().GetCache().Scan(cmd.maintainanceID)
 	if err != nil {
 		return err
 	}
@@ -144,11 +137,11 @@ func colourPass(ctx *appcontext.AppContext, repo *repository.Repository, cache *
 	// For now we keep the same serial so that those delete gets merged in.
 	// Once we do the real deletion we will rebuild the aggregated view
 	// excluding those ressources alltogether.
-	deltaState := repo.NewStateDelta(sc)
+	deltaState := cmd.repository.NewStateDelta(sc)
 
 	coloredPackfiles := 0
 	for packfile := range packfiles {
-		has, err := repo.HasDeletedPackfile(packfile)
+		has, err := cmd.repository.HasDeletedPackfile(packfile)
 		if err != nil {
 			return err
 		}
@@ -168,14 +161,14 @@ func colourPass(ctx *appcontext.AppContext, repo *repository.Repository, cache *
 		return err
 	}
 
-	if err := repo.PutState(identifier, buf); err != nil {
+	if err := cmd.repository.PutState(cmd.maintainanceID, buf); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func sweepPass(ctx *appcontext.AppContext, repo *repository.Repository, cache *caching.MaintainanceCache) error {
+func (cmd *Cleanup) sweepPass(ctx *appcontext.AppContext, cache *caching.MaintainanceCache) error {
 	// These need to be configurable per repo, but we don't have a mechanism yet (comes in a PR soon!)
 	cutoff := time.Now().AddDate(0, 0, -30)
 	doDeletion := false
@@ -183,7 +176,7 @@ func sweepPass(ctx *appcontext.AppContext, repo *repository.Repository, cache *c
 	// First go over all the packfiles coloured by first pass.
 	packfileremoved := 0
 	blobRemoved := 0
-	for packfileMAC, deletionTime := range repo.ListDeletedPackfiles() {
+	for packfileMAC, deletionTime := range cmd.repository.ListDeletedPackfiles() {
 		if deletionTime.After(cutoff) {
 			continue
 		}
@@ -193,16 +186,16 @@ func sweepPass(ctx *appcontext.AppContext, repo *repository.Repository, cache *c
 		// phase.
 		if cache.HasPackfile(packfileMAC) {
 			fmt.Fprintf(ctx.Stderr, "cleanup: Concurrent backup used %x, uncolouring the packfile.\n", packfileMAC)
-			repo.RemoveDeletedPackfile(packfileMAC)
+			cmd.repository.RemoveDeletedPackfile(packfileMAC)
 			continue
 		}
 
 		// First thing we remove the packfile entry from our state, this means
 		// that now effectively all of its blob are unreachable
-		if err := repo.RemovePackfile(packfileMAC); err != nil {
+		if err := cmd.repository.RemovePackfile(packfileMAC); err != nil {
 			fmt.Fprintf(ctx.Stderr, "cleanup: Failed to remove packfile %s from state\n", packfileMAC)
 		} else {
-			repo.RemoveDeletedPackfile(packfileMAC)
+			cmd.repository.RemoveDeletedPackfile(packfileMAC)
 		}
 
 		packfileremoved++
@@ -211,7 +204,7 @@ func sweepPass(ctx *appcontext.AppContext, repo *repository.Repository, cache *c
 	// Second garbage collect dangling blobs in our state. This is the blobs we
 	// just orphaned plus potential orphan blobs from aborted backups etc.
 	toDelete := map[objects.MAC]struct{}{}
-	for blob, err := range repo.ListOrphanBlobs() {
+	for blob, err := range cmd.repository.ListOrphanBlobs() {
 		if err != nil {
 			fmt.Fprintf(ctx.Stderr, "cleanup: Failed to fetch orphaned blob\n")
 			continue
@@ -219,20 +212,20 @@ func sweepPass(ctx *appcontext.AppContext, repo *repository.Repository, cache *c
 
 		blobRemoved++
 		toDelete[blob.Location.Packfile] = struct{}{}
-		if err := repo.RemoveBlob(blob.Type, blob.Blob, blob.Location.Packfile); err != nil {
+		if err := cmd.repository.RemoveBlob(blob.Type, blob.Blob, blob.Location.Packfile); err != nil {
 			// No hurt in this failing, we just have cruft left around, but they are unreachable anyway.
 			fmt.Fprintf(ctx.Stderr, "cleanup: garbage orphaned blobs pass failed to remove blob %x, type %s\n", blob.Blob, blob.Type)
 		}
 	}
 
 	fmt.Fprintf(ctx.Stdout, "cleanup: %d blobs and %d packfiles were removed\n", blobRemoved, packfileremoved)
-	if err := repo.PutCurrentState(); err != nil {
+	if err := cmd.repository.PutCurrentState(); err != nil {
 		return err
 	}
 
 	if doDeletion {
 		for packfileMAC := range toDelete {
-			if err := repo.DeletePackfile(packfileMAC); err != nil {
+			if err := cmd.repository.DeletePackfile(packfileMAC); err != nil {
 				fmt.Fprintf(ctx.Stderr, "cleanup: Sweep pass failed to delete packfile %x, skipping it\n", packfileMAC)
 			}
 		}
@@ -252,23 +245,36 @@ func (cmd *Cleanup) Execute(ctx *appcontext.AppContext, repo *repository.Reposit
 	// 5. remaining packfiles should be marked as deleted in the state
 	// 6. remove the packfile in repository once it's flagged as deleted AND all snapshots have been `snapshot.Check`-ed
 	// 7. rebuild a new aggregate state with a new serial without the deleted packfiles
+
 	cache, err := repo.AppContext().GetCache().Maintainance(repo.Configuration().RepositoryID)
 	if err != nil {
 		fmt.Fprintf(ctx.Stderr, "cleanup: Failed to open local cache %s\n", err)
 		return 1, err
 	}
 
-	if err := updateCache(ctx, repo, cache); err != nil {
+	cmd.repository = repo
+	// This random id generation for non snapshot state should probably be encapsulated somewhere.
+	n, err := rand.Read(cmd.maintainanceID[:])
+	if err != nil {
+		fmt.Fprintf(ctx.Stderr, "cleanup: Failed to read from random source.%s\n", err)
+		return 1, err
+	}
+	if n != len(cmd.maintainanceID) {
+		fmt.Fprintf(ctx.Stderr, "cleanup: Failed to read from random source.%s\n", err)
+		return 1, io.ErrShortWrite
+	}
+
+	if err := cmd.updateCache(ctx, cache); err != nil {
 		fmt.Fprintf(ctx.Stderr, "cleanup: Failed to update local cache %s\n", err)
 		return 1, err
 	}
 
-	if err := colourPass(ctx, repo, cache); err != nil {
+	if err := cmd.colourPass(ctx, cache); err != nil {
 		fmt.Fprintf(ctx.Stderr, "cleanup: Colouring pass failed %s\n", err)
 		return 1, err
 	}
 
-	if err := sweepPass(ctx, repo, cache); err != nil {
+	if err := cmd.sweepPass(ctx, cache); err != nil {
 		fmt.Fprintf(ctx.Stderr, "cleanup: Sweep pass failed %s\n", err)
 		return 1, err
 	}
