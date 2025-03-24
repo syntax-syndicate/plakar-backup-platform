d2d94e500fc6d24d327a7f958176b1a4f4dc4dad
Omar Polo <op@omarpolo.com>
2025-03-24 19:11:26 +0000

    tests: dedup generateSnapshot

--- cmd/plakar/subcommands/locate/locate_test.go
+++ cmd/plakar/subcommands/locate/locate_test.go
@@ -3,24 +3,13 @@ package locate
 import (
 	"bytes"
 	"encoding/hex"
-	"fmt"
-	"io"
 	"os"
 	"strings"
 	"testing"
 
-	"github.com/PlakarKorp/plakar/appcontext"
-	"github.com/PlakarKorp/plakar/caching"
-	"github.com/PlakarKorp/plakar/hashing"
-	"github.com/PlakarKorp/plakar/logging"
-	"github.com/PlakarKorp/plakar/repository"
-	"github.com/PlakarKorp/plakar/resources"
 	"github.com/PlakarKorp/plakar/snapshot"
 	_ "github.com/PlakarKorp/plakar/snapshot/exporter/fs"
-	"github.com/PlakarKorp/plakar/snapshot/importer/fs"
-	"github.com/PlakarKorp/plakar/storage"
-	bfs "github.com/PlakarKorp/plakar/storage/backends/fs"
-	"github.com/PlakarKorp/plakar/versioning"
+	ptesting "github.com/PlakarKorp/plakar/testing"
 	"github.com/stretchr/testify/require"
 )
 
@@ -29,83 +18,14 @@ func init() {
 }
 
 func generateSnapshot(t *testing.T, bufOut *bytes.Buffer, bufErr *bytes.Buffer) *snapshot.Snapshot {
-	// init temporary directories
-	tmpRepoDirRoot, err := os.MkdirTemp("", "tmp_repo")
-	require.NoError(t, err)
-	tmpRepoDir := fmt.Sprintf("%s/repo", tmpRepoDirRoot)
-	tmpCacheDir, err := os.MkdirTemp("", "tmp_cache")
-	require.NoError(t, err)
-	tmpBackupDir, err := os.MkdirTemp("", "tmp_to_backup")
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		os.RemoveAll(tmpRepoDir)
-		os.RemoveAll(tmpCacheDir)
-		os.RemoveAll(tmpBackupDir)
-		os.RemoveAll(tmpRepoDirRoot)
+	return ptesting.GenerateSnapshot(t, bufOut, bufErr, nil, []ptesting.MockFile{
+		ptesting.NewMockDir("subdir"),
+		ptesting.NewMockDir("another_subdir"),
+		ptesting.NewMockFile("subdir/dummy.txt", 0644, "hello dummy"),
+		ptesting.NewMockFile("subdir/foo.txt", 0644, "hello foo"),
+		ptesting.NewMockFile("subdir/to_exclude", 0644, "*/subdir/to_exclude\n"),
+		ptesting.NewMockFile("another_subdir/bar.txt", 0644, "hello bar"),
 	})
-	// create temporary files to backup
-	err = os.MkdirAll(tmpBackupDir+"/subdir", 0755)
-	require.NoError(t, err)
-	err = os.MkdirAll(tmpBackupDir+"/another_subdir", 0755)
-	require.NoError(t, err)
-	err = os.WriteFile(tmpBackupDir+"/subdir/dummy.txt", []byte("hello dummy"), 0644)
-	require.NoError(t, err)
-	err = os.WriteFile(tmpBackupDir+"/subdir/foo.txt", []byte("hello foo"), 0644)
-	require.NoError(t, err)
-	err = os.WriteFile(tmpBackupDir+"/subdir/to_exclude", []byte("*/subdir/to_exclude\n"), 0644)
-	require.NoError(t, err)
-	err = os.WriteFile(tmpBackupDir+"/another_subdir/bar", []byte("hello bar"), 0644)
-	require.NoError(t, err)
-
-	// create a storage
-	r, err := bfs.NewStore(map[string]string{"location": "fs://" + tmpRepoDir})
-	require.NotNil(t, r)
-	require.NoError(t, err)
-	config := storage.NewConfiguration()
-	serialized, err := config.ToBytes()
-	require.NoError(t, err)
-
-	hasher := hashing.GetHasher(hashing.DEFAULT_HASHING_ALGORITHM)
-	wrappedConfigRd, err := storage.Serialize(hasher, resources.RT_CONFIG, versioning.GetCurrentVersion(resources.RT_CONFIG), bytes.NewReader(serialized))
-	require.NoError(t, err)
-
-	wrappedConfig, err := io.ReadAll(wrappedConfigRd)
-	require.NoError(t, err)
-
-	err = r.Create(wrappedConfig)
-	require.NoError(t, err)
-
-	// open the storage to load the configuration
-	r, serializedConfig, err := storage.Open(map[string]string{"location": tmpRepoDir})
-	require.NoError(t, err)
-
-	// create a repository
-	ctx := appcontext.NewAppContext()
-	ctx.Stdout = bufOut
-	ctx.Stderr = bufErr
-	cache := caching.NewManager(tmpCacheDir)
-	ctx.SetCache(cache)
-
-	// Create a new logger
-	logger := logging.NewLogger(bufOut, bufErr)
-	logger.EnableInfo()
-	ctx.SetLogger(logger)
-	repo, err := repository.New(ctx, r, serializedConfig)
-	require.NoError(t, err, "creating repository")
-
-	// create a snapshot
-	snap, err := snapshot.New(repo)
-	require.NoError(t, err)
-	require.NotNil(t, snap)
-
-	imp, err := fs.NewFSImporter(map[string]string{"location": tmpBackupDir})
-	require.NoError(t, err)
-	snap.Backup(imp, &snapshot.BackupOptions{Name: "test_backup", MaxConcurrency: 1})
-
-	err = snap.Repository().RebuildState()
-	require.NoError(t, err)
-
-	return snap
 }
 
 func TestExecuteCmdLocateDefault(t *testing.T) {
