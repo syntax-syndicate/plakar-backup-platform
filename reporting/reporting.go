7b436cf8d0155cb93b32df7fd0a37f9b0fcde91d
Eric Faurot <eric@faurot.net>
2025-03-27 22:50:04 +0100

    split emitters into their own file

--- reporting/reporting.go
+++ reporting/reporting.go
@@ -1,15 +1,10 @@
 package reporting
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
-	"runtime"
 	"time"
 
-	"github.com/PlakarKorp/plakar/cmd/plakar/utils"
 	"github.com/PlakarKorp/plakar/logging"
 	"github.com/PlakarKorp/plakar/repository"
 	"github.com/PlakarKorp/plakar/snapshot/header"
@@ -144,53 +139,4 @@ func (reporter *Reporter) taskEnd(status TaskStatus, errorCode TaskErrorCode, er
 	reporter.currentSnapshot = nil
 	reporter.currentRepository = nil
 	reporter.emitter.Emit(report, reporter.logger)
-}
-
-type HttpEmitter struct {
-	url   string
-	retry uint8
-}
-
-func (emitter *HttpEmitter) Emit(report Report, logger *logging.Logger) {
-	data, err := json.Marshal(report)
-	if err != nil {
-		logger.Error("failed to encode report: %s", err)
-		return
-	}
-	for _ = range emitter.retry {
-		err := emitter.tryEmit(data)
-		if err == nil {
-			return
-		}
-		logger.Warn("failed to emit report: %s", err)
-	}
-	logger.Error("failed to emit report after %d tries", emitter.retry)
-}
-
-func (reporter *HttpEmitter) tryEmit(data []byte) error {
-	req, err := http.NewRequest("POST", reporter.url, bytes.NewReader(data))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("User-Agent", fmt.Sprintf("plakar/%s (%s/%s)", utils.VERSION, runtime.GOOS, runtime.GOARCH))
-	req.Header.Set("Content-Type", "application/json")
-
-	client := http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if 200 <= res.StatusCode && res.StatusCode < 300 {
-		return nil
-	}
-
-	return fmt.Errorf("request failed with status %s", res.Status)
-}
-
-type NullEmitter struct {
-}
-
-func (emitter *NullEmitter) Emit(report Report, logger *logging.Logger) {
 }
