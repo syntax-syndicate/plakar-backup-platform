4f9da82e30f774094451e77073bf862959e5792c
Omar Polo <op@omarpolo.com>
2025-04-18 13:08:18 +0000

    add a test

--- snapshot/snapshot_test.go
+++ snapshot/snapshot_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/PlakarKorp/plakar/objects"
 	"github.com/PlakarKorp/plakar/repository"
 	"github.com/PlakarKorp/plakar/snapshot"
 	ptesting "github.com/PlakarKorp/plakar/testing"
@@ -40,4 +41,67 @@ func TestSnapshot(t *testing.T) {
 
 	require.Equal(t, snap.Header.Identifier, snap2.Header.Identifier)
 	require.Equal(t, snap.Header.Timestamp.Truncate(time.Nanosecond), snap2.Header.Timestamp.Truncate(time.Nanosecond))
+}
+
+func checkmacs(t *testing.T, snap *snapshot.Snapshot) bool {
+	var (
+		indexContent   = map[objects.MACTuple]struct{}{}
+		crawledContent = map[objects.MACTuple]struct{}{}
+	)
+
+	var err error
+	for res, mac := range snap.MACs(&err) {
+		indexContent[objects.MACTuple{Resource: res, MAC: mac}] = struct{}{}
+	}
+	if err != nil {
+		t.Fatalf("snap.MACs failed with %v", err)
+	}
+
+	for res, mac := range snap.CrawlMACs(&err) {
+		crawledContent[objects.MACTuple{Resource: res, MAC: mac}] = struct{}{}
+	}
+	if err != nil {
+		t.Fatalf("snap.MACs failed with %v", err)
+	}
+
+	success := true
+	for tupl := range indexContent {
+		if _, ok := crawledContent[tupl]; !ok {
+			success = false
+			t.Errorf("MAC index yield but not found in crawling: %v %x",
+				tupl.Resource.String(), tupl.MAC)
+		}
+	}
+
+	for tupl := range crawledContent {
+		if _, ok := indexContent[tupl]; !ok {
+			success = false
+			t.Errorf("crawling yield but not found in MAC index: %v %x",
+				tupl.Resource.String(), tupl.MAC)
+		}
+	}
+
+	return success
+}
+
+func TestMACs(t *testing.T) {
+	repo := ptesting.GenerateRepository(t, nil, nil, nil)
+	snap1 := ptesting.GenerateSnapshot(t, repo, []ptesting.MockFile{
+		ptesting.NewMockFile("dummy.txt", 0644, "hello"),
+	})
+	defer snap1.Close()
+
+	if !checkmacs(t, snap1) {
+		t.Errorf("MACs and CrawlMACs don't agree!")
+	}
+
+	snap2 := ptesting.GenerateSnapshot(t, repo, []ptesting.MockFile{
+		ptesting.NewMockFile("dummy.txt", 0644, "hello"),
+		ptesting.NewMockFile("foo.txt", 0644, "foo bar baz"),
+	})
+	defer snap2.Close()
+
+	if !checkmacs(t, snap2) {
+		t.Errorf("MACs and CrawlMACs don't agree after incremental backup")
+	}
 }
