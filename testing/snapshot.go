2bc42a8c0a8417d5a497ae316240f0f621ae91c8
Mathieu Masson <mathieu.masson@plakar.io>
2025-04-28 11:02:52 +0200

    tests/ptar: Import the fsimporter by default.

--- testing/snapshot.go
+++ testing/snapshot.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"testing"
 
+	_ "github.com/PlakarKorp/plakar/snapshot/importer/fs"
+
 	"github.com/PlakarKorp/plakar/objects"
 	"github.com/PlakarKorp/plakar/repository"
 	"github.com/PlakarKorp/plakar/snapshot"
