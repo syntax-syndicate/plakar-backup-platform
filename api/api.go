3cadcdfc0c02c99a59f2f6ed53c0c25f527d86be
Gilles Chehade <gilles@poolp.org>
2025-05-09 13:25:06 +0200

    missing endpoint

--- api/api.go
+++ api/api.go
@@ -162,6 +162,7 @@ func SetupRoutes(server *http.ServeMux, repo *repository.Repository, token strin
 	server.Handle("GET /api/proxy/v1/account/notifications", authToken(JSONAPIView(servicesProxy)))
 	server.Handle("POST /api/proxy/v1/account/notifications/set-status", authToken(JSONAPIView(servicesProxy)))
 	server.Handle("GET /api/proxy/v1/account/services/alerting", authToken(JSONAPIView(servicesProxy)))
+	server.Handle("PUT /api/proxy/v1/account/services/alerting", authToken(JSONAPIView(servicesProxy)))
 	server.Handle("GET /api/proxy/v1/reporting/reports", authToken(JSONAPIView(servicesProxy)))
 
 	server.Handle("GET /api/repository/info", authToken(JSONAPIView(repositoryInfo)))
