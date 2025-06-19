6e49ed885e30af5b536afbafb5dbf399c48c73a6
Omar Polo <op@omarpolo.com>
2025-06-19 08:37:03 +0000

    actually, remove NormalizePath altogether
    It's only currently used on the os.Getwd() output and the reason
    it was added no longer holds.

--- main.go
+++ main.go
@@ -79,11 +79,6 @@ func EntryPoint() int {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		return 1
 	}
-	cwd, err = utils.NormalizePath(cwd)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return 1
-	}
 
 	opt_cpuDefault := runtime.GOMAXPROCS(0)
 	if opt_cpuDefault != 1 {
