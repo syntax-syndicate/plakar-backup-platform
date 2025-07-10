31f378998c6fd963537d80a3c596138713708145
Mathieu Masson <mathieu.masson@plakar.io>
2025-07-10 11:44:02 +0200

    sftp: Improvements to the sftp through ssh conn.
    * use options everywhere rather than playing tricks with an URI.

--- sftp/sftp.go
+++ sftp/sftp.go
@@ -36,12 +36,38 @@ type config struct {
 }
 
 func Connect(endpoint *url.URL, params map[string]string) (*sftp.Client, error) {
-	host := strings.TrimSuffix(endpoint.Host, ":")
-	if endpoint.User != nil {
-		host = endpoint.User.Username() + "@" + host
+	var args []string
+
+	// Due to the agent, we can't have anything interactive right now (password/known host check etc)
+	// so disable them to fail early and in a meaningful way.
+	args = append(args, "-o", "BatchMode=yes")
+
+	if params["insecure_ignore_host_key"] == "true" {
+		args = append(args, "-o", "StrictHostKeyChecking=no")
 	}
 
-	cmd := exec.Command("ssh", "-o", "BatchMode=yes", host, "-s", "sftp")
+	if id := params["identity"]; id != "" {
+		args = append(args, "-i", id)
+	}
+
+	if endpoint.User != nil && params["username"] != "" {
+		return nil, fmt.Errorf("can not use user@host foo syntax and username parameter.")
+	} else if endpoint.User != nil {
+		args = append(args, "-l", endpoint.User.Username())
+	} else if params["username"] != "" {
+		args = append(args, "-l", params["username"])
+	}
+
+	if endpoint.Port() != "" {
+		args = append(args, "-p", endpoint.Port())
+	}
+
+	args = append(args, endpoint.Hostname())
+
+	// This one must be after the host, tell the ssh command to load the sftp subsystem
+	args = append(args, "-s", "sftp")
+
+	cmd := exec.Command("ssh", args...)
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
 		return nil, err
@@ -51,6 +77,10 @@ func Connect(endpoint *url.URL, params map[string]string) (*sftp.Client, error) 
 	go func() {
 		sc := bufio.NewScanner(stderr)
 		for sc.Scan() {
+			if strings.HasPrefix(sc.Text(), "Warning:") {
+				continue
+			}
+
 			sshErr = fmt.Errorf("ssh command error: %q", sc.Text())
 		}
 	}()
